4ef4a789bce4d88e334c28bd6e3027479b3aed85
Rinat Uzanzaev <rinatuzanzaev13@gmail.com>
2023-09-04 16:21:20 +0300

    added kilometers to miles converter

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,18 @@ public class Main {
                 Welcome to Unit Converter!
                 1 - Convert miles to kilometers
                 2 - Convert kilometers to miles
-                Choose what to covert: """);
+                Choose what to covert:\040""");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
 
+    }
+
+    public static void kilometersToMiles() {
+        Scanner sc = new Scanner(System.in);
+        final double milesToKm = 0.621371;
+        System.out.print("Enter the number of kilometers: ");
+        double km = sc.nextDouble();
+        double miles = km * milesToKm;
+        System.out.printf("%f km = %f miles", km, miles);
     }
 }
