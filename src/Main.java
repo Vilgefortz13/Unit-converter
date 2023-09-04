6a4341a5293582c09547b66f3fa65866a6a1ee55
Rinat Uzanzaev <rinatuzanzaev13@gmail.com>
2023-09-04 16:13:51 +0300

    created the basic logic

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("""
+                Welcome to Unit Converter!
+                1 - Convert miles to kilometers
+                2 - Convert kilometers to miles
+                Choose what to covert: """);
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+    }
+}
